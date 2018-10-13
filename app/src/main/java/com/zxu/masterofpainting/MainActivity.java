51b969dca4d5d8b1bdc21331ee29c8aa5ef8a244
zxu <z18255713582@outlook.com>
2018-10-13 13:51:59 +0800

    takepicture

--- app/src/main/java/com/zxu/masterofpainting/MainActivity.java
+++ app/src/main/java/com/zxu/masterofpainting/MainActivity.java
@@ -1,13 +1,81 @@
 package com.zxu.masterofpainting;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+
+public class MainActivity extends Activity{
+    public static final int TAKE_PHOTO = 1;
+    private ImageView picture;
+    private Button takepicture;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        picture = (ImageView) findViewById(R.id.picture);
+        takepicture = (Button) findViewById(R.id.take_photo);
+        takepicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建File对象，用于存储拍照后的照片
+                File outputimage = new File(getExternalCacheDir(),"output_image.jpg");
+                try {
+                    if (outputimage.exists()) {
+                        outputimage.delete();
+                    }
+                    outputimage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUri = FileProvider.getUriForFile(MainActivity.this,
+                            "com.zxu.masterofpainting.fileprovider", outputimage);
+                } else {
+                    imageUri = Uri.fromFile(outputimage);
+                }
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        //将拍摄的照片显示出来
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                                .openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+                default:
+                    break;
+        }
     }
 }
