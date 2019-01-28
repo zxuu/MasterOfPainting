120b185bf6cc833a3356f5e0884c6bfbc118398d
zxu <z18255713582@outlook.com>
2019-01-28 17:27:50 +0800

    roll

--- app/src/main/java/com/zxu/masterofpainting/fragment/EdibleEfficacyFragment.java
+++ app/src/main/java/com/zxu/masterofpainting/fragment/EdibleEfficacyFragment.java
@@ -7,14 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.zxu.masterofpainting.R;
 
 public class EdibleEfficacyFragment extends Fragment {
+    private TextView edibleEffiTextView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edible_efficacy, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        edibleEffiTextView = (TextView) view.findViewById(R.id.edible_efficacy_text_view);
+        edibleEffiTextView.setText("dfsgj\nd\nsd\nsdf\nasfd\nadf\ner\ret\nsdf\nsdf\nsdf\nasde\nsad\nasd\nsadf\nwer\naf\nqwe\n" +
+                "ds\nasd\nert\nef\ndfg\nyuti\nsxzc\ndf\nsdf\nsdf\ndf\ndsfg\nsdf\newr\ncvb\ndfg\ndfg\new");
     }
 }
--- app/src/main/java/com/zxu/masterofpainting/fragment/SuitableAvoidFragment.java
+++ app/src/main/java/com/zxu/masterofpainting/fragment/SuitableAvoidFragment.java
@@ -7,15 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.zxu.masterofpainting.R;
 
 public class SuitableAvoidFragment extends Fragment {
+    private TextView suitableAvoidTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_suitable_avoid, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        suitableAvoidTextView = (TextView) view.findViewById(R.id.suitable_avoid_text_view);
+        suitableAvoidTextView.setText("dfsgj\nd\nsd\nsdf\nasfd\nadf\ner\ret\nsdf\nsdf\nsdf\nasde\nsad\nasd\nsadf\nwer\naf\nqwe\n" +
+                "ds\nasd\nert\nef\ndfg\nyuti\nsxzc\ndf\nsdf\nsdf\ndf\ndsfg\nsdf\newr\ncvb\ndfg\ndfg\new");
     }
 }
